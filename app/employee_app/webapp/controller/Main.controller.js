f5f1f7b01d478aa04adc348443a094518db42622
Minerva Jiménez Álvarez <minerva.jimenez@nubexx.es>
2026-05-07 09:59:32 +0000

    feat: employee app frontend created, no code yet

---
+++ app/employee_app/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.nbx.employeeapp.controller.Main", {
+        onInit() {
+        }
+    });
+});
--- app/services.cds
+++ app/services.cds
@@ -1,2 +1,4 @@
 
 using from './manager_app/annotations';
+
+using from './employee_app/annotations';
